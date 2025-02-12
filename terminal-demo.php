9531be48c3e153c7e4ac241b9e331cc37db598ed
adeleyeayodeji <biodunhi@gmail.com>
2025-02-12 16:05:43 +0100

    Added index file

---
+++ terminal-demo.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Plugin Name: Terminal Demo
+ * Plugin URI:  https://terminal.africa
+ * Author:      Terminal Africa
+ * Author URI:  https://terminal.africa
+ * Description: This plugin imports demo data for the Terminal Africa Hub theme
+ * Version:     0.1.0
+ * License:     GPL-2.0+
+ * License URL: http://www.gnu.org/licenses/gpl-2.0.txt
+ * text-domain: terminal-demo
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+//silence is golden
